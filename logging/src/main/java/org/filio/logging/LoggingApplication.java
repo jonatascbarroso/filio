d4b04f36f4755174b558db6270b6da972179ee68
Jonatas <jonatas@gmx.net>
2019-09-20 15:29:48 -0300

    updating the package name on logging module

--- logging/src/main/java/org/filio/logging/LoggingApplication.java
+++ logging/src/main/java/org/filio/logging/LoggingApplication.java
@@ -1,4 +1,4 @@
-package org.fts.logging;
+package org.filio.logging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- logging/src/test/java/org/filio/logging/LoggingApplicationTests.java
+++ logging/src/test/java/org/filio/logging/LoggingApplicationTests.java
@@ -1,4 +1,4 @@
-package org.fts.logging;
+package org.filio.logging;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
