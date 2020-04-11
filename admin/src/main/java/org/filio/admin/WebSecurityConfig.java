bae718b37f9f617b5ec6ac3d30917783ff3a2854
Jonatas <jonatas@gmx.net>
2020-04-10 23:26:41 -0300

    restarting with new version

--- admin/src/main/java/org/filio/admin/WebSecurityConfig.java
+++ admin/src/main/java/org/filio/admin/WebSecurityConfig.java
@@ -1,19 +1,59 @@
 package org.filio.admin;
 
+import java.util.UUID;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final AdminServerProperties adminServer;
+
+    public WebSecurityConfig(AdminServerProperties adminServer) {
+        this.adminServer = adminServer;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        String adminContextPath = this.adminServer.getContextPath();
+
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
         http
             .authorizeRequests()
-            .anyRequest()
-            .permitAll();
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage(adminContextPath + "/login")
+                .successHandler(successHandler)
+                .and()
+            .logout()
+                .logoutUrl(adminContextPath + "/logout")
+                .and()
+            .httpBasic()
+                .and()
+            .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers(
+                    adminContextPath + "/instances",
+                    adminContextPath + "/actuator/**")
+                .and()
+            .rememberMe()
+                .key(UUID.randomUUID().toString())
+                .tokenValiditySeconds(1209600);
     }
 }
