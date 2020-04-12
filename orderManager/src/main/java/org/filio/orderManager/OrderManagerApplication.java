94dd8b482aa6ab7e1d183937124fb63aefc7eb4b
Jonatas <jonatas@gmx.net>
2020-04-11 23:31:26 -0300

    order manager

---
+++ orderManager/src/main/java/org/filio/orderManager/OrderManagerApplication.java
@@ -0,0 +1,15 @@
+package org.filio.orderManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagerApplication.class, args);
+	}
+
+}
---
+++ orderManager/src/test/java/org/filio/orderManager/OrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.filio.orderManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
